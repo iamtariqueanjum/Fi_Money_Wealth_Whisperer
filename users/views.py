56b40932f1c4658f2bdb268aa3501db9347dc1b7
iamtariqueanjum <tariqueanjummd9302@gmail.com>
2025-07-26 15:29:46 +0530

    Logout module done

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, reverse
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 # Create your views here.
@@ -14,3 +14,8 @@ def login_view(request):
         else:
             messages.error(request, 'Invalid username or password')
     return render(request, 'users/login.html')
+
+def logout_view(request):
+    if request.method == 'POST':
+        # logout(request)
+        return redirect('login')
